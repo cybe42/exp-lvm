f2bbc94328743b9904fa1fad8184b750686ca078
cybe <47478929+cybe42@users.noreply.github.com>
2025-03-02 03:48:40 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,1 @@
-return loadstring(game.HttpService:GetAsync("https://github.com/cybe42/exp-lvm/raw/refs/heads/main/SLVM.lua"))()
+return loadstring(game:HttpGet("https://github.com/cybe42/exp-lvm/raw/refs/heads/main/SLVM.lua"))()
