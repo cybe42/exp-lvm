56bdbe02dec30c25b1dd15a636c3ea94d2b2b753
cybe <47478929+cybe42@users.noreply.github.com>
2025-03-02 03:39:12 +0000

    Update Enum.lua

--- Enum.lua
+++ Enum.lua
@@ -20,7 +20,7 @@
 	SLVM | By the ASense Team
 ]]
 
-local Types = require(script.Parent:WaitForChild("Types"))
+local Types = loadstring(game.HttpService:GetAsync("https://github.com/cybe42/exp-lvm/raw/refs/heads/main/Types.lua"))()
 local EnumList: Types.SLVMEnum = shared.SLVMEnums or {}
 
 --// Lazy Functions
